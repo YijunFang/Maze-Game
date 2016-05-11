8b913917939bb1eef8d3e83054d6fadcd418b067
Weilon <weilony@hotmail.com>
2016-05-11 14:40:46 +1000

    Game will generate 2D representation of maze according to GameState.

--- src/main/java/GameRep/Game.java
+++ src/main/java/GameRep/Game.java
@@ -1,45 +1,87 @@
 package GameRep;
 
-import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import MazeRep.AdjacencyListGraphNode;
-import MazeRep.ExposedGraphMaze;
-import MazeRep.GraphMaze;
-import MazeRep.Maze;
-import MazeRep.MazeFactory;
-import MazeRep.Node;
-import MazeRep.MazeGenStrategy.MazeGenStrategy;
-import MazeRep.MazeGenStrategy.RandomisedRecursiveDFS;
+import Common.CoordinatePair;
+import Common.Difficulty;
 
-public class Game<T> {
-    JFrame frame;
-    Node<T>[][] nodes;
-    Maze<T> maze;
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+    static final int squareLength = 10;
+    int mazeLength;
     
-    public Game(int height, int width) {
-        frame = new JFrame();
-        frame.setBounds(400, 400, 0, 0);
-        frame.setLocationRelativeTo(null);
-        maze = new MazeFactory().generateMaze(GraphMaze.class, width, height, 1);
-        
-        System.out.println(maze.getHeight());
-        System.out.println(maze.getLength());
+    int x = 0;
+    int y = 0;
+    
+    private void moveBall() {
+        x = x + 1;
+        y = y + 1;
     }
     
-    public static void main (String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Game<AdjacencyListGraphNode> window = new Game<AdjacencyListGraphNode>(30, 30);
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
+    Square[][] maze;
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        //draw the squares
+        for (int down = 0; down < maze.length; down++) {
+            for (int across = 0; across < maze[down].length; across++) {
+                int pixelX = across * squareLength;
+                int pixelY =   down * squareLength;
+                
+                Square s = maze[down][across];
+                if (s.isBorderedOn(SquareSide.UP)) {
+                    g2d.draw(new Line2D.Double(pixelX, pixelY, pixelX + squareLength, pixelY));
+                }
+                if (s.isBorderedOn(SquareSide.RIGHT)) {
+                    g2d.draw(new Line2D.Double(pixelX + squareLength, pixelY, pixelX + squareLength, pixelY + squareLength));
+                }
+                if (s.isBorderedOn(SquareSide.DOWN)) {
+                    g2d.draw(new Line2D.Double(pixelX, pixelY + squareLength, pixelX + squareLength, pixelY + squareLength));
+                }
+                if (s.isBorderedOn(SquareSide.LEFT)) {
+                    g2d.draw(new Line2D.Double(pixelX, pixelY, pixelX, pixelY + squareLength));
                 }
             }
-        });
-        Stopwatch s = new Stopwatch();
+        }
     }
     
+    public void start(Difficulty diff) {
+        setSize(500, 500);
+        GameState gs = new GameState(diff);
+        
+        //Get maze length
+        mazeLength = diff.getSideLength();
+        mazeLength = 10; //stub
+        
+        //Initialise maze representation
+        for (int down = 0; down < mazeLength; down++) {
+            for (int across = 0; across < mazeLength; across++) {
+                maze[down][across] = gs.getSquareAt(new CoordinatePair (down, across));
+            }
+        }
+        
+        repaint();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Maze");
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        game.start(Difficulty.EASY);
+    }
 }
