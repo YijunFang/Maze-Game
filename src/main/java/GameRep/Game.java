34dcbf3617deb92ce73c243ed6a7f34b3141d3f0
Weilon <weilony@hotmail.com>
2016-05-13 17:21:38 +1000

    Improved player animation (still not finished properly yet)

--- src/main/java/GameRep/Game.java
+++ src/main/java/GameRep/Game.java
@@ -6,28 +6,33 @@ import java.awt.Graphics2D;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import Common.CoordinatePair;
 import Common.Difficulty;
 
 @SuppressWarnings("serial")
 public class Game extends JPanel {
-    static final int squareLength = 50;
-    int mazeLength;
+    private static final int squareLength = 50;
+    private int mazeLength;
+    private boolean playerPlaced = false;
     GameState gs;
+    Square[][] maze;
+    
+    private double playerLocationX = 0;
+    private double playerLocationY = 0;
     
     public Game() {
         initKeyPressDetect(); //debug
     }
-
-    
-    Square[][] maze;
     
     @Override
     public void paint(Graphics g) {
@@ -44,6 +49,8 @@ public class Game extends JPanel {
                 int pixelY =   down * squareLength;
                 
                 Square s = maze[down][across];
+                
+                //Draw square borders
                 if (s.isBorderedOn(SquareSide.UP)) {
                     g2d.draw(new Line2D.Double(pixelX, pixelY, pixelX + squareLength, pixelY));
                 }
@@ -60,10 +67,20 @@ public class Game extends JPanel {
         }
         
         //draw character
-        CoordinatePair playerLoc = gs.getPlayerPosition();
-        System.out.println("PlayerLocation = x: " + playerLoc.across + " y: " + playerLoc.down);
-        Rectangle2D player = new Rectangle2D.Double(
-                playerLoc.across * squareLength + 2.5, playerLoc.down * squareLength + 2.5, squareLength - 5, squareLength - 5);
+        //System.out.println("PlayerLocation = x: " + playerLoc.across + " y: " + playerLoc.down); //debug
+        int playerSize = squareLength - 5;
+        Rectangle2D player;
+        
+        if (!playerPlaced) { //if player not placed, determine its initial location
+            CoordinatePair playerLoc = gs.getPlayerPosition();
+            playerLocationX = playerLoc.across * squareLength + 2.5;
+            playerLocationY = playerLoc.down * squareLength + 2.5;
+            playerPlaced = true;
+        }
+        
+        player = new Rectangle2D.Double(
+                playerLocationX, playerLocationY, playerSize, playerSize);
+        
         g2d.setPaint(Color.red);
         g2d.fill(player);
     }
@@ -102,13 +119,65 @@ public class Game extends JPanel {
         Square currentSquare = gs.getSquareAt(playerPosition);
         if (!currentSquare.isBorderedOn(SquareSide.DOWN)) {
             setNewPlayerPosition (new CoordinatePair(playerPosition.down + 1, playerPosition.across));
+            CoordinatePair playerLoc = gs.getPlayerPosition();
+            double newPlayerLocationY = playerLoc.down * squareLength + 2.5;
+            
+            Timer moveTimer = new Timer(1, null);
+            Timer repaintTimer = new Timer (15, null);
+            ActionListener moveAction = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (newPlayerLocationY < playerLocationY - 2.5) {
+                        moveTimer.stop();
+                        repaintTimer.stop();
+                        repaint();
+                    }
+                    playerLocationY+=1;
+                }
+                
+            };
+            ActionListener repaintAction = new ActionListener() {
+                @Override
+                public void actionPerformed (ActionEvent e) {
+                    repaint();
+                }
+            };
+            moveTimer.addActionListener(moveAction);
+            repaintTimer.addActionListener(repaintAction);
+            
+            moveTimer.setInitialDelay(0);
+            moveTimer.start(); 
+            repaintTimer.start();
+            
         }
     }
     public void keyPressedLeft() {
         CoordinatePair playerPosition = gs.getPlayerPosition();
         Square currentSquare = gs.getSquareAt(playerPosition);
+        
         if (!currentSquare.isBorderedOn(SquareSide.LEFT)) {
             setNewPlayerPosition (new CoordinatePair(playerPosition.down, playerPosition.across - 1));
+            
+            CoordinatePair playerLoc = gs.getPlayerPosition();
+            double newPlayerLocationX = playerLoc.across * squareLength + 2.5;
+            
+            Timer timer = new Timer(1, null);
+            ActionListener timerAction = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (newPlayerLocationX > playerLocationX) {
+                        timer.stop();
+                    }
+                    playerLocationX-=2;
+                    repaint();
+                }
+                
+            };
+            timer.addActionListener(timerAction);
+            
+            timer.setInitialDelay(0);
+            timer.start(); 
+            repaint();
         }
     }
     public void keyPressedUp() {
@@ -116,6 +185,25 @@ public class Game extends JPanel {
         Square currentSquare = gs.getSquareAt(playerPosition);
         if (!currentSquare.isBorderedOn(SquareSide.UP)) {
             setNewPlayerPosition (new CoordinatePair(playerPosition.down - 1, playerPosition.across));
+            CoordinatePair playerLoc = gs.getPlayerPosition();
+            double newPlayerLocationY = playerLoc.down * squareLength + 2.5;
+            
+            Timer timer = new Timer(1, null);
+            ActionListener timerAction = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (newPlayerLocationY > playerLocationY) {
+                        timer.stop();
+                    }
+                    playerLocationY-=2;
+                    repaint();
+                }
+                
+            };
+            timer.addActionListener(timerAction);
+            
+            timer.setInitialDelay(0);
+            timer.start(); 
         }
     }
     public void keyPressedRight() {
@@ -123,12 +211,46 @@ public class Game extends JPanel {
         Square currentSquare = gs.getSquareAt(playerPosition);
         if (!currentSquare.isBorderedOn(SquareSide.RIGHT)) {
             setNewPlayerPosition (new CoordinatePair(playerPosition.down, playerPosition.across + 1));
+
+            CoordinatePair playerLoc = gs.getPlayerPosition();
+            double newPlayerLocationX = playerLoc.across * squareLength + 2.5;
+            
+            Timer timer = new Timer(1, null);
+            ActionListener timerAction = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (newPlayerLocationX < playerLocationX) {
+                        timer.stop();
+                    }
+                    playerLocationX+=2;
+                    repaint();
+                }
+                
+            };
+            timer.addActionListener(timerAction);
+            
+            timer.setInitialDelay(0);
+            timer.start(); 
+            repaint();
         }
     }
     
     private void setNewPlayerPosition (CoordinatePair newLocation) {
         gs.setPlayerPosition(newLocation);
+        /*
+        CoordinatePair playerLoc = gs.getPlayerPosition();
+        double newPlayerLocationX = playerLoc.across * squareLength + 2.5;
+        double newPlayerLocationY = playerLoc.down * squareLength + 2.5;
+        
+        while (newPlayerLocationX != playerLocationX || newPlayerLocationY != playerLocationY) {
+            if (newPlayerLocationX != playerLocationX) {
+                playerLocationX += (newPlayerLocationX - playerLocationX)/10;
+            }
+            repaint();
+        }
+        
         repaint();
+        */
     }
     
     public void hintCoinActivated() {
