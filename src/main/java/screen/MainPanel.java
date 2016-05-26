63c47b27136a42fe07f9a2dab397f509fa2f7119
YIJUN <1015585546@qq.com>
2016-05-26 11:52:28 +1000

    some more buttons

--- src/main/java/screen/MainPanel.java
+++ src/main/java/screen/MainPanel.java
@@ -240,10 +240,10 @@ public class MainPanel extends JPanel {
 		component.setOpaque(false);
 		component.setLayout(new GridLayout(0, 1, 10, 10));
 
-		component.add(new Button("Easy", this, null, 600, 75));
-		component.add(new Button("Medium", this, null, 600, 75));
+		component.add(new Button("Easy", this, "easy.png", 600, 75));
+		component.add(new Button("Medium", this, "medium.png", 600, 75));
 		component.add(new Button("Hard", this, "hard.png", 600, 75));
-		component.add(new Button("Main Menu", this, null, 600, 75));
+		component.add(new Button("Main Menu", this, "mainmenu.png", 600, 75));
 
 		GroupLayout gl_newGame = new GroupLayout(newGame);
 		gl_newGame
